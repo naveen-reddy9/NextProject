a63234e5fb51c3977aae086cd61d639bfa983b3e
naveen-reddy9 <130099342+naveen-reddy9@users.noreply.github.com>
2024-05-14 07:48:46 -0400

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,32 +1,47 @@
-"use client";
+const getPredictedAge = async (name: string) => {
+  const res = await fetch (`https://api.agify.io/?name=${name}`);
+  return res.json()
+};
 
-import { useState, FormEvent } from "react"; 
-import {useRouter} from 'next/navigation'
-export default function Home() {
-  const [inputVal, setInputVal] = useState("")
-const {push} = useRouter()
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    push(`/prediction/${inputVal}`);
-  };
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="p-4 shadow-md bg-white rounded-md">
-        <h1 className="text-2xl font-semibold mb-4 text-black"> 
-        Enter Your Name
-        </h1>
+const getPredictedGender = async (name: string) => {
+  const res = await fetch (`https://api.genderize.io/?name=${name}`);
+  return res.json()
+};
+
+const getPredictedCountry = async (name: string) => {
+  const res = await fetch (`https://api.nationalize.io/?name=${name}`);
+  return res.json()
+};
+
+
+interface Params {
+  params: { name: string };
+}
+
+export default async function Page({ params }: Params) {
+  const ageData = getPredictedAge(params.name)
+  const genderData = getPredictedGender(params.name)
+  const countryData = getPredictedCountry(params.name)
+
+  const [age, gender, country] = await Promise.all([
+    ageData, 
+    genderData, 
+    countryData
+  ]);
+    return (
+      <div>
+        <div>
+          <div> Personal Info</div>
+          <div> Age: {age?.age}</div>
+          <div> Gender: {gender?.gender}</div>
+          <div> Country: {country?.country[0]?.country_id}</div>
+        </div>
       </div>
-      <form onSubmit={handleSubmit} className="space-y-3">
-        <input 
-        type="text" 
-        value={inputVal}
-        onChange={(e) => setInputVal(e.target.value)}
-        className="w-full p-2 border border-gray-300 rounded text-black"
-        placeholder="Type your name..." />
-        <button type="submit"
-        className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-        > Submit</button>
-      </form>
-    </div>
-  );
+    )
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        {""} 
+        {params.name}
+        </main>
+    );
 }
